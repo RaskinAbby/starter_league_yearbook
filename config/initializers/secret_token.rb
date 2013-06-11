a25495f5c75d0dfcfd6c61da808ef9f25b2df11d
Abby Raskin <abbyraskin@gmail.com>
2013-06-11 12:56:21 -0500

    generated new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StarterLeagueYearbook::Application.config.secret_token = '7dadc9359a13bbc028893ab0e1cd61d36bb45492e7b8cd494d0b9deb29fec6a3d3e58e8684c634156b7a14ac95e6da037d974a43fca374502661f8ce86c2225a'
