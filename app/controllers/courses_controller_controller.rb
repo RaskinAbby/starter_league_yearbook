e0de73bf22624b91dc55608ce9d2669904d6b47f
Abby Raskin <abbyraskin@gmail.com>
2013-06-11 13:21:27 -0500

    fixed some errors in route and model

--- app/controllers/courses_controller_controller.rb
+++ app/controllers/courses_controller_controller.rb
@@ -1,5 +1,6 @@
 class CoursesControllerController < ApplicationController
-course_number = params[:course_id]
-@students = Student.fetch(course_number)
-@course_title = @students.first.section
+def section
+  course_number = params[:course_id]
+  @students = Student.fetch(course_number)
+  @course_title = @students.first.section
 end
